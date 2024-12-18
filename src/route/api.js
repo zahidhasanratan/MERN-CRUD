c8a743d18c61aabe206bd512b01900f9566763e5
esoftzahid <esoft123456@gmail.com>
2024-12-18 14:55:28 +0600

    Created api.js for Routing

--- src/route/api.js
+++ src/route/api.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const ProductsController = require('../controller/ProductsController');
+
 const router = express.Router();
+
+//C=Create
+router.post("/CreateProduct",ProductsController.CreateProducts);
+
+//R=Read
+router.get("/ReadProduct",ProductsController.ReadProducts);
+
+//U=Update
+router.post("/UpdateProduct/:id",ProductsController.UpdateProducts);
+
+//D=Delete
+router.get("/DeleteProduct/:id",ProductsController.DeleteProducts);
 
 //API Routing End Point 
 
