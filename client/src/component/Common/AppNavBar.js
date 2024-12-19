37b5a4f8009556f12e74a68436bb713ea470f857
esoftzahid <esoft123456@gmail.com>
2024-12-19 16:05:56 +0600

    Create Form Created

--- client/src/component/Common/AppNavBar.js
+++ client/src/component/Common/AppNavBar.js
@@ -4,25 +4,20 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavLink } from 'react-router';
 
 export default class AppNavBar extends Component {
   render() {
     return (
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">CRUD</Navbar.Brand>
+          <Navbar.Brand>CRUD</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link><NavLink to="/">List</NavLink></Nav.Link>
+              <Nav.Link><NavLink to="/create">Create</NavLink></Nav.Link>
+              
             </Nav>
           </Navbar.Collapse>
         </Container>
--- client/src/component/Create/CreateForm.js
+++ client/src/component/Create/CreateForm.js
@@ -2,9 +2,39 @@ import React from "react";
 
 const CreateForm = () => {
   return (
-    <div>
-      <h1>Create Form</h1>
-      {/* Add form fields or other content here */}
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4 p-2">
+          <label>Product Name</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="col-md-4 p-2">
+          <label>Product Code</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="col-md-4 p-2">
+          <label>Image</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="col-md-4 p-2">
+          <label>Unit Price</label>
+          <input type="number" className="form-control" />
+        </div>
+        <div className="col-md-4 p-2">
+          <label>QTY</label>
+          <input type="number" className="form-control" />
+        </div>
+        <div className="col-md-4 p-2">
+          <label>Total Price</label>
+          <input type="number" className="form-control" />
+        </div>
+      </div>
+      <br />
+      <div className="row">
+        <div className="col-md-4 p-2">
+          <button className="btn btn-primary w-100">Save</button>
+        </div>
+      </div>
     </div>
   );
 };
